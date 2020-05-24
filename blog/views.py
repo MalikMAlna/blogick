4432c14482f865e3bdb4301853ed196b67a4f6ed
Mike A <jimalik2525@gmail.com>
2020-05-24 14:54:31 -0400

    Added stub data to play around with on rendered homepage

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Mike A.',
+        'title': 'First Blog Post',
+        'content': 'blah blah blah blah',
+        'date_posted': 'May 24, 2020'
+    },
+    {
+        'author': 'Hakim D.',
+        'title': 'Blog Post 2',
+        'content': 'blah blah blah blah',
+        'date_posted': 'May 24, 2020'
+    }
+]
+
 
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
