4e7f5c1c3c4afb86294af5e63bfb16f3b29ba4d6
Mike A <jimalik2525@gmail.com>
2020-08-02 14:38:51 -0400

    Fixed field name issue

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(auto_now_add=True)
     # can do default=timezone.now argument to allow for changeable date_posted
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_last_modifed = models.DateTimeField(auto_now=True)
+    date_last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
