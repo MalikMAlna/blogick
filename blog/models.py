d9a9ed6483cd3e063dacc8c439e757fc22d931c0
Mike A <jimalik2525@gmail.com>
2020-08-02 14:10:48 -0400

    Created Post Model and added migrations to gitignore

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+# from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    # can do default=timezone.now argument to allow for changeable date_posted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_last_modifed = models.DateTimeField(auto_now=True)
