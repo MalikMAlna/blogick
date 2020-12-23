913947f43f7d12b86cf585bdce661f4dd3fb2d91
Mike A <jimalik2525@gmail.com>
2020-12-23 16:01:00 -0500

    Adding customized fields for username and display name

--- authentication/forms.py
+++ authentication/forms.py
@@ -10,6 +10,16 @@ class RegistrationForm(UserCreationForm):
     email = forms.EmailField(label="Enter Email",
                              max_length=60,
                              help_text="Required. Please enter a valid email")
+    username = forms.CharField(
+        label="Username",
+        max_length=25,
+        help_text="""Required. Now enter a valid and unique username"""
+    )
+    display_name = forms.CharField(
+        label="Display Name",
+        max_length=25,
+        help_text="""Required. And also enter a unique display name"""
+    )
 
     class Meta:
         model = Account
