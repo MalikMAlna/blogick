a4173dcd61dc2a4420411a093ad0463f563e5b73
Mike A <jimalik2525@gmail.com>
2020-12-25 15:03:25 -0500

    Adding PostListView url pattern to act as new homepage and creating PostDetailView

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # posts_stub = [
@@ -17,15 +17,26 @@ from .models import Post
 #     }
 # ]
 
-
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, "blog/home.html", context)
+# Function View Example for Home
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, "blog/home.html", context)
 
 
 class PostListView(ListView):
+    model = Post
+    # Default template_name is <app_name>/<model_name>_<viewtype>.html
+    template_name = 'blog/home.html'
+    # Now setting context, default context_object_name is object_list
+    # for ListView
+    context_object_name = 'posts'
+    # Newest to oldest by date posted
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
     model = Post
 
 
