926ca093d42ab8ec1699bc19a363cb0d5748ce15
Mike A <jimalik2525@gmail.com>
2020-12-25 14:47:44 -0500

    Adding PostListView, going to start using more django generic class-based views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # posts_stub = [
@@ -22,6 +23,10 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
 
 
 def about(request):
